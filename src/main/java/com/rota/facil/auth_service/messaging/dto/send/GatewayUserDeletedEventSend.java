f8981d8c53fc93e1513c0937f6baf7681893c405
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-04-15 06:16:21 -0300

    feat: add more information in delete user event to gateway

--- src/main/java/com/rota/facil/auth_service/messaging/dto/send/GatewayUserDeletedEventSend.java
+++ src/main/java/com/rota/facil/auth_service/messaging/dto/send/GatewayUserDeletedEventSend.java
@@ -1,7 +1,10 @@
 package com.rota.facil.auth_service.messaging.dto.send;
 
 
+import java.util.UUID;
+
 public record GatewayUserDeletedEventSend(
+        UUID userId,
         String userToken
 ) {
 }
