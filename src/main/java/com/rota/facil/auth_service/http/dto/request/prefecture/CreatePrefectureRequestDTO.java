524d20b521df3259b102b450f6bc63af33575fd6
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-04-28 13:43:55 -0300

    refactor: adjusting authentication filter

--- src/main/java/com/rota/facil/auth_service/http/dto/request/prefecture/CreatePrefectureRequestDTO.java
+++ src/main/java/com/rota/facil/auth_service/http/dto/request/prefecture/CreatePrefectureRequestDTO.java
@@ -2,14 +2,16 @@ package com.rota.facil.auth_service.http.dto.request.prefecture;
 
 import com.rota.facil.auth_service.domain.enums.Region;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreatePrefectureRequestDTO(
         @NotBlank(message = "nome da prefeitura é obrigatório")
         String name,
 
-        @NotBlank(message = "região é obrigatório")
+        @NotNull(message = "região é obrigatório")
         Region region,
 
+        @NotNull(message = "usuario default da prefeitura eh obrigatório")
         PrefectureUser prefectureUser
 ) {
 }
