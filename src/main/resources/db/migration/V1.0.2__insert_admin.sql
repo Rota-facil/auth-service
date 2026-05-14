a6acf74b4a3ae03da799453cb67258dabcfd782c
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-05-14 09:22:18 -0300

    refactor: adjusting inserts

--- src/main/resources/db/migration/V1.0.2__insert_admin.sql
+++ src/main/resources/db/migration/V1.0.2__insert_admin.sql
@@ -1,11 +1,21 @@
--- a senha eh admin
+-- a senha eh senha
 
 INSERT INTO users_tb (user_id, prefecture_id, name, email, password, cpf, role) VALUES (
     gen_random_uuid(),
     (SELECT p.prefecture_id FROM prefectures_tb p WHERE p.name = 'São Paulo' ),
     'tulipa',
     'tulipa@gmail.com',
-    '$2a$10$CmSHeq5j1Skg3m/s9kuVFe/HByL/iKI3NqRpVTlWIJN9/N.StLvC6',
+    '$2a$10$xi8X1eELxbfKvWH26z2aKO.liFbyp.Iatyoh/wm6NHYd./nLAQVQG',
     '000.000.000-00',
     'SUPERUSER'
-)
+);
+
+INSERT INTO users_tb (user_id, prefecture_id, name, email, password, cpf, role) VALUES (
+    gen_random_uuid(),
+    (SELECT p.prefecture_id FROM prefectures_tb p WHERE p.name = 'São Paulo' ),
+    'admin_sao_paulo',
+    'admin_sao_paulo@gmail.com',
+    '$2a$10$xi8X1eELxbfKvWH26z2aKO.liFbyp.Iatyoh/wm6NHYd./nLAQVQG',
+    '000.000.000-01',
+    'ADMIN'
+);
