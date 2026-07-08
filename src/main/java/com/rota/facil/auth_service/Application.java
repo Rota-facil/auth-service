0b64158f55f2c6e75cd663831c937cc866b77f38
Thauã Gabriel <tagashi.dev@gmail.com>
2026-07-08 02:35:56 -0300

    feat: add annotation to spring data web support

--- src/main/java/com/rota/facil/auth_service/Application.java
+++ src/main/java/com/rota/facil/auth_service/Application.java
@@ -2,8 +2,12 @@ package com.rota.facil.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(
+		pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 public class Application {
 
 	public static void main(String[] args) {
